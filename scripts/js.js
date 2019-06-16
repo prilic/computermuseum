08db3411846c25aa7f68b84dd4f6819bd54f4587
prilic <47575156+prilic@users.noreply.github.com>
2019-06-16 22:42:15 +0200

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -1,6 +1,5 @@
 
 //Menu
-alert('hello');
 function toggle(){
   var a = document.getElementsByClassName('menu');
 
@@ -12,7 +11,7 @@ function toggle(){
   	}
   }
   else{
-  	
+
   	for(i=0;i<a.length;i++)
   	{
   		a[i].style.display="none";
@@ -31,3 +30,103 @@ document.getElementsByClassName('menu')[1].style.display="none";
 document.getElementsByClassName('menu')[2].style.display="none";
 document.getElementsByClassName('menu')[3].style.display="none";
 */
+
+//Galerie
+
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[0].style.display=="block")
+    {
+    picHidden[0].style.display="none";
+    }
+    else
+    {
+    picHidden[0].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[1].style.display=="block")
+    {
+    picHidden[1].style.display="none";
+    }
+    else
+    {
+    picHidden[1].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[2].style.display=="block")
+    {
+    picHidden[2].style.display="none";
+    }
+    else
+    {
+    picHidden[2].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[3].style.display=="block")
+    {
+    picHidden[3].style.display="none";
+    }
+    else
+    {
+    picHidden[3].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[4].style.display=="block")
+    {
+    picHidden[4].style.display="none";
+    }
+    else
+    {
+    picHidden[4].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[5].style.display=="block")
+    {
+    picHidden[5].style.display="none";
+    }
+    else
+    {
+    picHidden[5].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[6].style.display=="block")
+    {
+    picHidden[6].style.display="none";
+    }
+    else
+    {
+    picHidden[6].style.display="block";
+    }
+}
+function pictureHidden()
+{
+    var picHidden=document.getElementsByClassName("pictureHidden")
+    if(picHidden[7].style.display=="block")
+    {
+    picHidden[7].style.display="none";
+    }
+    else
+    {
+    picHidden[7].style.display="block";
+    }
+}
+//Controle de formulaire
